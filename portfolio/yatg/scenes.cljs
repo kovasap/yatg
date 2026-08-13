2f8f92c5f55624bf1d9c198cdddca7d6e84cf64f
Kovas Palunas <kovas.palunas@gmail.com>
2026-08-12 21:43:45 -0700

    working portfolio battle

--- portfolio/yatg/scenes.cljs
+++ portfolio/yatg/scenes.cljs
@@ -5,10 +5,12 @@
             [yatg.ui.battle :refer [render-battle]]
             [yatg.battle :refer [generate-battle]]
             [yatg.character :refer [generate-character]]
+            [yatg.event-handling.infra]
+            [yatg.event-handling.actions]
             [nexus.registry :as nxr]
             [replicant.dom :as r]
             [dataspex.core :as dataspex]
-            [malli.dev.cljs :as dev]
+            [malli.dev.cljs :as malli-dev]
             [malli.dev.pretty :as pretty]))
 
 (defscene overworld
@@ -26,17 +28,19 @@
                   :id :idle}]
     :id         :assassin}])
 
-(defscene
-  battle
-  (render-battle
-    (generate-battle 3 3 [(generate-character :they true sprite-templates)])))
+(defscene battle
+          :params
+          (atom
+            (let [characters [(generate-character :they true sprite-templates)]
+                  battle     (generate-battle 3 3 characters)]
+              {:current-scene {:battle battle} :characters characters}))
+          [store portfolio-opts]
+          (dataspex/inspect "Game state" store)
+          (r/set-dispatch! #(nxr/dispatch store %1 %2))
+          (render-battle (:battle (:current-scene @store))))
                            
-(def store (atom nil))
-  
 (defn main []
-  (dataspex/inspect "Game state" store)
-  (dev/start! {:report (pretty/reporter)})
-  (r/set-dispatch! #(nxr/dispatch store %1 %2))
+  (malli-dev/start! {:report (pretty/reporter)})
   (portfolio/start!
    {:config
     {:css-paths ["/styles.css"]
